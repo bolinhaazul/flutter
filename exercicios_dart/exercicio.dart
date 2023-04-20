5d1518e903788af737cb58bb350625f554da9e71
bolinhaazul <caioandradesje@gmail.com>
2023-04-20 13:19:08 -0300

    aula 37

---
+++ exercicios_dart/exercicio.dart
@@ -0,0 +1,8 @@
+soma(int a, int b) {
+  return a + b;
+}
+
+main() {
+  final r = soma(6, 10);
+  print(r);
+}
--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,1 +1,47 @@
+import 'package:flutter/material.dart';
+
+main() => (runApp(const PerguntaApp()));
+
+class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+
+  void responder() {
+    print('vc clicou ai meu bacana');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qal sua cor favorita?',
+      'quantos kg de mandioca tem na roca?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: avoid_print
 
